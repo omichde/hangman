ecf23909a67f68efe8ac77d18ba2959c55cfedfc
Oliver Michalak <oliver@werk01.de>
2021-06-12 13:00:32 +0200

    Initial Commit

---
+++ hangman/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hangman
+//
+//  Created by Oliver Michalak on 12.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
